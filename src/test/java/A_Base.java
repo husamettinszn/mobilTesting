a84bcb24e901718aab0aa492419de01c7ba95b04
husamettinszn <husamettinszn@gmail.com>
2021-03-18 20:46:20 +0100

    w

--- src/test/java/A_Base.java
+++ src/test/java/A_Base.java
@@ -9,18 +9,40 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class A_Base {
+/*
     @Test
     public void test() throws MalformedURLException, InterruptedException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-         desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability(CapabilityType.PLATFORM_NAME ,"Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidEmulator");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Husamettin\\Downloads\\GestureTool (1).apk");
+        desiredCapabilities.setCapability("appPackage", "com.davemac327.gesture.tool");
+        desiredCapabilities.setCapability("appActivity", "com.davemac327.gesture.tool.GestureBuilderActivity");
+        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, false);
+        desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 15);
+        AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
+        Thread.sleep(5000);
+
+    }
+
+ */
+    @Test
+    public void test() throws MalformedURLException, InterruptedException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndoidEmulator");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidEmulator");
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
-        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Husamettin\\Downloads\\GestureTool.apk");
-
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Husamettin\\Downloads\\GestureTool (1).apk");
+        desiredCapabilities.setCapability("appPackage","com.davemac327.gesture.tool");
+        desiredCapabilities.setCapability("appActivity","com.davemac327.gesture.tool.GestureBuilderActivity");
+        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET,true);
+        desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,15);
         AndroidDriver<WebElement> driver  = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"),desiredCapabilities);
         Thread.sleep(5000);
     }
+
 }
