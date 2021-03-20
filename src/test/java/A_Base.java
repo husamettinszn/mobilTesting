8cdb7fffb99d85cb336c767c7ef5a240c16c28aa
husamettinszn <husamettinszn@gmail.com>
2021-03-20 19:33:29 +0100

    w

--- src/test/java/A_Base.java
+++ src/test/java/A_Base.java
@@ -1,48 +1,37 @@
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Driver;
 
 public class A_Base {
-/*
+
     @Test
     public void test() throws MalformedURLException, InterruptedException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidEmulator");
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        //desiredCapabilities.setCapability("chromedriverExecutable", "C:\\Users\\Husamettin\\Downloads\\chromedriver.exe");
+        //desiredCapabilities.setCapability("appPackage", "com.google.android.dialer");
+        //desiredCapabilities.setCapability("appActivity", "com.google.android.dialer.extensions.GoogleDialtactsActivity");
         desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Husamettin\\Downloads\\GestureTool (1).apk");
-        desiredCapabilities.setCapability("appPackage", "com.davemac327.gesture.tool");
+        desiredCapabilities.setCapability("appPackage","com.davemac327.gesture.tool");
         desiredCapabilities.setCapability("appActivity", "com.davemac327.gesture.tool.GestureBuilderActivity");
-        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, false);
+        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, true);
         desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 15);
         AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
         Thread.sleep(5000);
 
-    }
 
- */
-    @Test
-    public void test() throws MalformedURLException, InterruptedException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidEmulator");
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
-        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Husamettin\\Downloads\\GestureTool (1).apk");
-        desiredCapabilities.setCapability("appPackage","com.davemac327.gesture.tool");
-        desiredCapabilities.setCapability("appActivity","com.davemac327.gesture.tool.GestureBuilderActivity");
-        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET,true);
-        desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,15);
-        AndroidDriver<WebElement> driver  = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"),desiredCapabilities);
-        Thread.sleep(5000);
     }
 
 }
