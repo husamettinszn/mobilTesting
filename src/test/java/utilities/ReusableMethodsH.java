e0af466697183ad5190984d3eb559c4514a0feae
husamettinszn <husamettinszn@gmail.com>
2021-04-10 21:03:04 +0200

    2

--- src/test/java/utilities/ReusableMethodsH.java
+++ src/test/java/utilities/ReusableMethodsH.java
@@ -1,11 +1,14 @@
 package utilities;
 
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class ReusableMethodsH {
 
+
     AndroidDriver driver = (AndroidDriver) Driver.getAppiumDriver();
 
     WebElement degisken = driver.findElementByXPath("");
+    WebElement getDegisken= driver.findElement(By.xpath("w"));
 }
